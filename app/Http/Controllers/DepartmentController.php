4fab43efe8f7c8853bb6b4cf576c0998d7c8c87e
rirakib <rirrakib10@gmail.com>
2022-02-17 00:37:07 +0600

    first commit

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Department;
+use Illuminate\Http\Request;
+
+class DepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $department = Department::all();
+        return view('backend.department.index',compact('department'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('backend.department.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $data = $request->all();
+        Department::create($data);
+        return redirect()->back()->with('stutus','Department created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Department  $department
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Department $department)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Department  $department
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Department $department)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Department  $department
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Department $department)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Department  $department
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $department = Department::find($id);
+        $department->delete();
+        return redirect()->back()->with('delete','Data has been deleted');
+    }
+}
---
+++ resources/views/backend/component/sidebar.blade.php
@@ -0,0 +1,133 @@
+<div class="col-md-3 left_col">
+    <div class="left_col scroll-view">
+        <div class="navbar nav_title" style="border: 0;">
+            <a href="" class="site_title"><i class="fa fa-paw"></i> <span>Dashboard</span></a>
+        </div>
+
+        <div class="clearfix"></div>
+
+        <!-- menu profile quick info -->
+        <div class="profile clearfix">
+            <div class="profile_pic">
+                <img src="{{asset('backend/images/img.jpg')}}" alt="..." class="img-circle profile_img">
+            </div>
+            <div class="profile_info">
+                <span>Welcome,</span>
+                <h2>username</h2>
+            </div>
+        </div>
+        <!-- /menu profile quick info -->
+
+        <br />
+        
+        <!-- sidebar menu -->
+        <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+            <div class="menu_section">
+                <div class="menu_section">
+                    <ul class="nav side-menu">
+                        <li><a href="{{route('dashboard.index')}}">Dashboard</a></li>
+                    </ul>
+                </div>
+
+                <h3>Students</h3>
+                <ul class="nav side-menu">
+                    <li><a><i class="fas fa-hourglass"></i> Department <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="{{route('department.index')}}">Index</a></li>
+                            <li><a href="{{route('department.create')}}">Create</a></li>
+                        </ul>
+                    </li>
+
+                    {{-- <li><a><i class="fas fa-book"></i> Subject <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="{{route('subject.index')}}">Index</a></li>
+                            <li><a href="{{route('subject.create')}}">Create</a></li>
+                        </ul>
+                    </li>
+                    <li><a><i class="fas fa-user-astronaut"></i> Group <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="{{route('group.index')}}">Index</a></li>
+                            <li><a href="{{route('group.create')}}">Create</a></li>
+                        </ul>
+                    </li>
+                    <li><a><i class="fas fa-user-md"></i> Student type <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="{{route('student-type.index')}}">Index</a></li>
+                            <li><a href="{{route('student-type.create')}}">Create</a></li>
+                        </ul>
+                    </li>
+                    <li><a><i class="fas fa-clock"></i> Shift <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="{{route('shift.index')}}">Index</a></li>
+                            <li><a href="{{route('shift.create')}}">Create</a></li>
+                        </ul>
+                    </li>
+                    <li><a><i class="fas fa-user-graduate"></i> Student <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="{{route('student.index')}}">Index</a></li>
+                            <li><a href="{{route('student.create')}}">Create</a></li>
+                        </ul>
+                    </li> --}}
+                    
+
+
+                </ul>
+            </div>
+
+            {{-- <div class="menu_section">
+                <h3>Official</h3>
+                <ul class="nav side-menu">
+                    <li><a><i class="fas fa-chalkboard-teacher"></i> Teacher <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="{{route('teacher.index')}}">Index</a></li>
+                            <li><a href="{{route('teacher.create')}}">Create</a></li>
+                        </ul>
+                    </li>
+                    <li><a><i class="fas fa-chalkboard-teacher"></i> Stafs <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="{{route('type.index')}}">Stafs type</a></li>
+                            <li><a href="{{route('staf.index')}}">Index</a></li>
+                            <li><a href="{{route('staf.create')}}">Create</a></li>
+                        </ul>
+                    </li>
+
+
+                    <li><a><i class="fas fa-chalkboard-teacher"></i> Attendence <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="{{route('attendance.student')}}">Student</a></li>
+                            <li><a href="{{route('attendance.teacher')}}">Teacher</a></li>
+                            <li><a href="{{route('attendance.staf')}}">Staf</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div> --}}
+
+
+            <div class="menu_section">
+                <h3></h3>
+                <ul class="nav side-menu">
+                    <!-- this is another section  -->
+                </ul>
+            </div>
+
+        </div>
+        <!-- /sidebar menu -->
+
+        <!-- /menu footer buttons -->
+        <div class="sidebar-footer hidden-small">
+            <a data-toggle="tooltip" data-placement="top" title="Settings">
+                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+            </a>
+            <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+                <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+            </a>
+            <a data-toggle="tooltip" data-placement="top" title="Lock">
+                <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+            </a>
+            <a data-toggle="tooltip" data-placement="top" title="Logout" href="">
+                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+            </a>
+        </div>
+        <!-- /menu footer buttons -->
+    </div>
+</div>
